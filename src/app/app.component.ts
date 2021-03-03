41824da954c260c396d3dbe17740d5982fb25282
Karthik <karthik@isquarebs.com>
2021-03-03 17:35:04 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'billing';
+ fitems:any; 
+  list: any =[];
+  gnumber: any;
+  hprice: any;
+  total=0;
+  
+ save(){
+  let f = {
+    items : this.fitems,
+    number : this.gnumber,
+    price : this.hprice,
+    total:this.total
+  };
+  this.list.push(f);
+  this.ftotal();
+}
+  
+  ftotal(){
+    this.total= this.gnumber*this.hprice;
+    console.log(this.total)
+  }
+
+  delete(list: any) {
+    const index = this.list.indexOf(list);
+    // tslint:disable-next-line:align
+    this.list.splice(index, 1);
+  }
 }
